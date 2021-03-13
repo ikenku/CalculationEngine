6057e4ea0e1ccb7b454bebbcf104492532ed860c
ikenku <58195237+ikenku@users.noreply.github.com>
2021-03-13 21:27:50 +0000

    Calculation engine 30% percent done

---
+++ CalculationEngine/Controllers/CalculationController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using CalculationEngine.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+using System.Data;
+using System;
+using System.Linq;
+
+namespace CalculationEngine.Controllers
+{
+    [Route("api/[controller]/")]
+    [ApiController]
+    public class CalculationController : ControllerBase
+    {
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Please enter your input string after the URL";
+        }
+
+        // GET api/<CalculationController>/5
+        [HttpGet("{input}")]
+        public double Get(string input)
+        {
+            char[] operators = { '/', '*', '+', '-' };
+
+            Calculation calc = new Calculation();
+
+            calc.NumbersAndOperatorsList = new List<string>(Regex.Split(input, @"([^\d\.]+)"));
+
+            foreach (char op in operators)
+            {
+                for (int i = 0; i < calc.NumbersAndOperatorsList.Count - 2; i += 2)
+                {
+                    if (!string.IsNullOrEmpty(calc.NumbersAndOperatorsList[i]) && !string.IsNullOrEmpty(calc.NumbersAndOperatorsList[i + 1]) && !string.IsNullOrEmpty(calc.NumbersAndOperatorsList[i + 2]))
+                    {
+                        calc.Operator = Convert.ToChar(calc.NumbersAndOperatorsList[i + 1]);
+                        if (!operators.Contains(calc.Operator))
+                            throw new ArithmeticException("Input expression is not valid - The operator is not in the required format of 'd', 'm', 'a', or 's'");
+                        if (op == calc.Operator)
+                        {
+                            calc.LeftOperand = Convert.ToDouble(calc.NumbersAndOperatorsList[i]);
+                            calc.RightOperand = Convert.ToDouble(calc.NumbersAndOperatorsList[i + 2]);
+                            calc.NumbersAndOperatorsList.RemoveAt(i + 1);
+                            calc.NumbersAndOperatorsList.RemoveAt(i + 1);
+                            DataTable dt = new DataTable();
+                            switch (calc.Operator)
+                            {
+
+                                case '/':
+                                    calc.Result = Convert.ToDouble(dt.Compute(calc.LeftOperand.ToString() + '/' + calc.RightOperand.ToString(), ""));
+                                    break;
+                                case '*':
+                                    calc.Result = Convert.ToDouble(dt.Compute(calc.LeftOperand.ToString() + '*' + calc.RightOperand.ToString(), ""));
+                                    break;
+                                case '+':
+                                    calc.Result = Convert.ToDouble(dt.Compute(calc.LeftOperand.ToString() + '+' + calc.RightOperand.ToString(), ""));
+                                    break;
+                                case '-':
+                                    calc.Result = Convert.ToDouble(dt.Compute(calc.LeftOperand.ToString() + '-' + calc.RightOperand.ToString(), ""));
+                                    break;
+                            }
+                            calc.NumbersAndOperatorsList.RemoveAt(i);
+                            calc.NumbersAndOperatorsList.Insert(i, calc.Result.ToString());
+                        }
+                    }
+                    else
+                        throw new ArithmeticException("Input expression is not valid - The operand is not valid");
+                }
+            }
+            return Convert.ToDouble(calc.NumbersAndOperatorsList[0]);
+
+        }
+
+    }
+}
---
+++ CalculationEngine/Models/Calculation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace CalculationEngine.Models
+{
+    public class Calculation
+    {
+        public double Result { get; set; }
+        public char Operator { get; set; }
+        public double LeftOperand { get; set; }
+        public double RightOperand { get; set; }
+        public List<string> NumbersAndOperatorsList { get; set; }
+    }
+}
