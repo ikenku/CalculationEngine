e409daf764eca01d7a36ddadad6a795987fbc50d
ikenku <58195237+ikenku@users.noreply.github.com>
2021-03-14 18:54:18 +0000

    Final version of Calculation Engine with client

---
+++ Controllers/CalculationController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using CalculationEngine.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+
+namespace CalculationEngine.Controllers
+{
+    [Route("api/[controller]/")]
+    [ApiController]
+    public class CalculationController : ControllerBase
+    {
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Please enter your input string after the URL";
+        }
+
+        // GET api/<CalculationController>/5
+        [HttpGet("{input}")]
+        public double Get(string input)
+        {
+            char[] operators = { 'd', 'm', 'a', 's' };
+
+            Calculation calc = new Calculation();
+
+            calc.NumbersAndOperatorsList = new List<string>(Regex.Split(input, @"([^\d\.]+)"));
+
+            return CalculationService.Evaluate(calc, operators);
+
+        }
+
+    }
+}
